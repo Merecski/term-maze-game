0662dbc80f90f705425bca8d5eb6b66c5e4c241f
Merecski <mikemerecki@gmail.com>
2020-12-15 23:14:45 -0500

    Breadth First Search algorithm maze generator
    Algorithm to generate and update cells accordingly to generate a length
    limited cell stack to search for the end and fill the grid.
    
    Limiting the stack size and repicking prevents one single path
    dominating the maze space without having any intersections.

--- maze.py
+++ maze.py
@@ -32,10 +32,57 @@ gui_encode = {
 class Maze:
     def __init__(self, size):
         self.size = size
+        self.start = (0, random.randint(0, self.size - 1))
+        self.end =   (self.size - 1, random.randint(0, self.size - 1))
+        self.grid = []
 
-    def generateMaze(self):
-        self.grid = self.generateBlank()
-        self.linkCells(self.grid, self.size)
+    def __str__(self):
+        string = '  ' * self.start[1] + 'S\n'
+        for row in self.grid:
+            tmp = ''
+            for cell in row:
+                tmp += gui_encode[cell.walls ^ 0b1111]
+            string += tmp + '\n'
+        string += '  ' * self.end[1] + 'E'
+        return string
+
+    def returnCell(self, pos):
+        return self.grid[pos[0]][pos[1]]
+
+    def algorithmInit(self):
+        root = self.returnCell(self.start)
+        root.knockWall(0b1000)
+        main_cell_stack = [root]
+        while main_cell_stack:
+            cell = random.choice(main_cell_stack)
+            if cell.hasNeighbors():
+                main_cell_stack += (self.algorithmShortWalk(cell))
+            else:
+                main_cell_stack.remove(cell)
+
+    def algorithmShortWalk(self, root):
+        max_trail = 2 * self.size // 3
+        cell_stack = [root]
+        while cell_stack and len(cell_stack) < max_trail:
+            free_cells = cell_stack[-1].hasNeighbors()
+            curr_cell = cell_stack[-1]
+            if free_cells:
+                face, next_cell = random.choice(free_cells)
+                curr_cell.knockWall(face)
+                if next_cell == self.returnCell(self.end):
+                    next_cell.knockWall(0b0100)
+                    return cell_stack
+                else:
+                    cell_stack.append(next_cell)
+            else:
+                cell_stack.pop()
+        return cell_stack
+
+    def indexGrid(self, grid, y, x):
+        if (y < 0 or x < 0) or (y >= len(grid) or x >= len(grid)):
+            return None
+        else:
+            return grid[y][x]
 
     def generateBlank(self):
         grid = []
@@ -46,11 +93,10 @@ class Maze:
             grid.append(tmp)
         return grid
 
-    def indexGrid(self, grid, y, x):
-        if (y < 0 or x < 0) or (y >= len(grid) or x >= len(grid)):
-            return None
-        else:
-            return grid[y][x]
+    def generateMaze(self):
+        self.grid = self.generateBlank()
+        self.linkCells(self.grid, self.size)
+        self.algorithmInit()
 
     def linkCells(self, grid, size):
         for y in range(size):
@@ -70,7 +116,31 @@ class Cell:
     def setNeighbor(self, face, cell):
         if cell: self.neighbors[face] = cell
 
+    def hasNeighbors(self):
+        avaliable = []
+        for face, cell in self.neighbors.items():
+            if cell.isEmpty():
+                avaliable.append((face, cell))
+        return avaliable
+
+    def isEmpty(self):
+        return self.walls == 0b1111
+
+    def knockWall(self, wall):
+        self.walls ^= wall
+        if wall > 0b0010:
+            inv_wall = 0b1100 ^ wall
+        else:
+            inv_wall = 0b0011 ^ wall
+        if wall in self.neighbors:
+            self.neighbors[wall].walls ^= inv_wall
+
 
 if __name__ == '__main__':
-    m = Maze(10)
+    try:
+        size = int(sys.argv[1])
+    except:
+        size = 10
+    m = Maze(size)
     m.generateMaze()
+    print(m)
